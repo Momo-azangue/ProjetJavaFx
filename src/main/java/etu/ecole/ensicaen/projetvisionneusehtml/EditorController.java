1ff0949ebd6707b575b8368c1a721d00698b7f2a
Momo-azangue <azanguewill@gmail.com>
2024-11-17 23:26:22 +0100

    changement

--- src/main/java/etu/ecole/ensicaen/projetvisionneusehtml/EditorController.java
+++ src/main/java/etu/ecole/ensicaen/projetvisionneusehtml/EditorController.java
@@ -6,6 +6,9 @@ import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
+import javafx.stage.FileChooser;
+
+import java.io.*;
 
 public class EditorController {
 
@@ -50,24 +53,20 @@ public class EditorController {
     @FXML
     public void initialize() {
 
-        Enregistrer.setDisable(true);
+       Enregistrer.setDisable(true);
         Enregistrersous.setDisable(true);
         Saveasbtn.setDisable(true);
         Savebtn.setDisable(true);
         Undobtn.setDisable(true);
         Redobtn.setDisable(true);
+
         WebEngine webEngine = webView.getEngine(); /* lier la première TextArea Statique à la Webview correspondante */
         textArea.textProperty().addListener((observable, oldValue, newValue) -> {
             webEngine.loadContent(newValue);
             EncoursdeRedaction = true;
             tab.setText("Untitled Tab 1 (*) ");
             CompteDesCarateres(textArea, caracteres,lignes);
-            Enregistrer.setDisable(false);
-            Enregistrersous.setDisable(false);
-            Saveasbtn.setDisable(false);
-            Savebtn.setDisable(false);
-            Undobtn.setDisable(false);
-            Redobtn.setDisable(false);
+            ControlButton();
         });
 
         Tab addTab = new Tab("+"); //Créer l'onglet "Ajouter" (avec le symbole "+")
@@ -114,12 +113,7 @@ public class EditorController {
             webEngine.loadContent(newValue);
             EncoursdeRedaction = true;
             newTab.setText("Untitled Tab " + tabCount + " (*) ");
-            Enregistrer.setDisable(false);
-            Enregistrersous.setDisable(false);
-            Savebtn.setDisable(false);
-            Saveasbtn.setDisable(false);
-            Undobtn.setDisable(false);
-            Redobtn.setDisable(false);
+            ControlButton();
             CompteDesCarateres(textArea, caracteres,lignes);
 
         });
@@ -144,6 +138,10 @@ public class EditorController {
     }
 
 
+
+
+
+// fonction pour compter les lignes et les caractères
     private void CompteDesCarateres(TextArea textArea, Label caracteres, Label lignes){
 
              textArea.textProperty().addListener((observable, oldValue, newValue) -> {
@@ -159,6 +157,161 @@ public class EditorController {
 
     }
 
+//fonction de controle de boutton
+    private   void ControlButton(){
+        if (EncoursdeRedaction = true ){
+
+            Enregistrer.setDisable(false);
+            Enregistrersous.setDisable(false);
+            Saveasbtn.setDisable(false);
+            Savebtn.setDisable(false);
+            Undobtn.setDisable(false);
+            Redobtn.setDisable(false);
+
+        }
+
+
+}
+
+    //méthode pour sauvegarder le fichcier actuel (si ouvert )
+    @FXML
+    private void handleSave() {
+        Tab selectedTab = tabPane.getSelectionModel().getSelectedItem();
+
+        // Vérifier si c'est l'onglet statique
+        if (selectedTab != null && selectedTab.getText().equals("Static Tab")) {
+            // Sauvegarder le contenu du TextArea1 de l'onglet statique
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Enregistrer sous");
+            fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Fichiers HTML", "*.html", "*.htm"));
+            File file = fileChooser.showSaveDialog(null);
+
+            if (file != null) {
+                saveFile(file, textArea.getText());
+            }
+        } else if (selectedTab != null && selectedTab.getUserData() != null) {
+            // Sauvegarder un fichier existant dans un onglet dynamique
+            File file = (File) selectedTab.getUserData();
+            saveFile(file, textArea.getText());
+        } else {
+            handleSaveAs();  // Sauvegarder un nouvel onglet dynamique sans fichier
+        }
+    }
+    @FXML
+    private void handleSaveAs() {
+        Tab selectedTab = tabPane.getSelectionModel().getSelectedItem();
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Enregistrer sous");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Fichiers HTML", "*.html", "*.htm"));
+        File file = fileChooser.showSaveDialog(null);
+
+        if (file != null) {
+            if (selectedTab.getText().equals("Static Tab")) {
+                // Sauvegarder le contenu de l'onglet statique
+                saveFile(file, textArea.getText());
+            } else {
+                // Sauvegarder le contenu d'un onglet dynamique
+                saveFile(file, textArea.getText());
+                selectedTab.setText(file.getName()); // Mettre à jour le nom de l'onglet
+                selectedTab.setUserData(file);  // Associer le fichier à l'onglet dynamique
+            }
+        }
+    }
+
+
+
+    @FXML
+    private void handleOpenFile(){
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Ouvrir un fichier HTML");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("fichier HTML", "*.html", "*.htm", "*.txt")
+        );
+        File file = fileChooser.showOpenDialog(null);
+
+        if(file != null){
+            ChargerUnFichierDansUneTab(file);
+        }
+    }
+
+
+
+    // Méthode pour enregistrer le contenu dans un fichier
+    private void saveFile(File file, String content) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            writer.write(content); // Sauvegarder le contenu de la TextArea dans le fichier
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private void ChargerUnFichierDansUneTab(File file){
+
+        try{
+
+            //nous commençons par lire le fichier du fichier HTML
+            StringBuilder contentBuilder = new StringBuilder();
+
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))){
+                String line;
+                while ((line = reader.readLine()) != null){
+                    contentBuilder.append(line).append("\n");
+                }
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            // Créer un nouvel onglet
+            Tab newTab = new Tab(file.getName());
+            newTab.setClosable(true);
+
+            // Créer un AnchorPane pour contenir la TextArea et la WebView
+            AnchorPane anchorPane = new AnchorPane();
+            SplitPane splitPane = new SplitPane();
+            TextArea textArea = new TextArea();
+            WebView webView = new WebView();
+            WebEngine webEngine = webView.getEngine();
+
+            // Charger le contenu HTML dans la TextArea
+            String fileContent = contentBuilder.toString();
+            textArea.setText(fileContent);
+
+            // Mettre à jour le WebView lorsque le contenu du TextArea change
+            textArea.textProperty().addListener((observable, oldValue, newValue) -> {
+                webEngine.loadContent(newValue);// Met à jour le rendu HTML en direct
+            });
+
+            // Charger initialement le contenu HTML dans le WebView
+            webEngine.loadContent(fileContent);
+
+            // Ajouter le TextArea et le WebView dans le SplitPane
+            splitPane.getItems().addAll(textArea, webView);
+            // Positionner le SplitPane dans l'AnchorPane comme dans le FXML
+            AnchorPane.setTopAnchor(splitPane, 3.0);
+            AnchorPane.setLeftAnchor(splitPane, 15.0);
+            AnchorPane.setRightAnchor(splitPane, 15.0);
+            AnchorPane.setBottomAnchor(splitPane, 23.0);
+
+            // Ajouter le SplitPane dans l'AnchorPane
+            anchorPane.getChildren().add(splitPane);
+
+            // Définir l'AnchorPane comme contenu de l'onglet
+            newTab.setContent(anchorPane);
+
+            // Ajouter l'onglet au TabPane
+            tabPane.getTabs().add(newTab);
+            tabPane.getSelectionModel().select(newTab); // Sélectionner automatiquement le nouvel onglet
+
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
 
 
 
--- src/main/java/etu/ecole/ensicaen/projetvisionneusehtml/HelloApplication.java
+++ src/main/java/etu/ecole/ensicaen/projetvisionneusehtml/HelloApplication.java
@@ -2,6 +2,7 @@ package etu.ecole.ensicaen.projetvisionneusehtml;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -12,6 +13,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
+        EditorController editorController = fxmlLoader.getController();
         stage.setScene(scene);
         stage.show();
     }
