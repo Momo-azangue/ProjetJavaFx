4a2c2fbd66b7309f35b82de617cb8f87b24c1b3e
Momo-azangue <azanguewill@gmail.com>
2024-11-18 02:11:24 +0100

    fini

--- src/main/java/etu/ecole/ensicaen/projetvisionneusehtml/HelloApplication.java
+++ src/main/java/etu/ecole/ensicaen/projetvisionneusehtml/HelloApplication.java
@@ -4,9 +4,12 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class HelloApplication extends Application {
     @Override
@@ -15,6 +18,27 @@ public class HelloApplication extends Application {
         Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
         EditorController editorController = fxmlLoader.getController();
         stage.setScene(scene);
+        stage.setTitle("Confirmation avant fermeture");
+        stage.setOnCloseRequest(windowEvent -> {
+
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Confirmation avant fermeture");
+            alert.setHeaderText("voulez vous vraiment fermer l'application ?");
+            alert.setContentText("Cliquez sur oui pour fermer ou sur Non pour annuler.");
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if(result.isPresent() && result.get() == ButtonType.OK){
+
+            System.out.println("Fermeture confirmée.");
+        }else{
+            
+            windowEvent.consume();
+            System.out.println("Fermeture annulée");
+        }
+
+
+
+        });
         stage.show();
     }
 
